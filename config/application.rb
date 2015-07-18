d4e7f87b4d2ddbc8448e11b4889dd0171e3a0be0
Christian Rolle <rolle.christian@gmail.com>
2015-07-18 17:58:08 +0200

    Adds path to fonts to asset pipe line.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Cr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Adds path to fonts to asset pathes
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
