3b68c11b06b3182c65e0cd012c54d9473fccbd22
Christian Rolle <rolle.christian@gmail.com>
2015-09-11 10:30:32 +0200

    Renamed Articles#index route to 'blog'.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911072539) do
+ActiveRecord::Schema.define(version: 20150911081526) do
 
-  create_table "blog_posts", force: :cascade do |t|
-    t.datetime "published_at", null: false
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "published_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
