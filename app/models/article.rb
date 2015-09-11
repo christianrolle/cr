ad45b54587d89551af7548153077198f3a69e483
Christian Rolle <rolle.christian@gmail.com>
2015-09-11 12:57:21 +0200

    Casts publishing time into date.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
   validates :title, presence: true, length: { in: 3..200 }
+
+  def published_on
+    published_at.to_date
+  end
 end
