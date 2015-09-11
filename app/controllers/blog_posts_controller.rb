3910e8934359bc081ee862c1a88e56cfb6eb3808
Christian Rolle <rolle.christian@gmail.com>
2015-09-11 10:23:18 +0200

    Added Articles resource.

--- app/controllers/blog_posts_controller.rb
+++
@@ -1,5 +0,0 @@
-class BlogPostsController < ApplicationController
-  def index
-    @blog_posts = BlogPost.all
-  end
-end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BlogPostsControllerTest < ActionController::TestCase
+class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
