5bb601232f9c3a8660038b68bab043630d1c15cb
Christian Rolle <rolle.christian@gmail.com>
2015-09-29 17:17:56 +0200

    Removed Devise and added Authlogic.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :articles, path: 'blog', only: [:index, :show]
-
+  match 'login', to: "user_sessions#new", via: :get
+  match 'logout', to: "user_sessions#destroy", via: :get
   root 'welcome#index'
+
+  resources :articles, path: 'blog', only: [:index, :show]
+  namespace :admin do
+    resources :articles, except: :show
+  end
 end
