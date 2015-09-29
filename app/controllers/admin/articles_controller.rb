7a10a924c9b98a024d2aee332d2a7e8d9225fbc4
Christian Rolle <rolle.christian@gmail.com>
2015-09-29 20:21:42 +0200

    Articles adminstration controller contains API for listing, editing, creating and destroying articles.

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,2 +1,18 @@
 class Admin::ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def edit
+    @article = Article.find params[:id]
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+  end
 end
