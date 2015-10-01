4ad905fba42cbeaf469c79620e940345085dcc85
Christian Rolle <rolle.christian@gmail.com>
2015-10-01 21:24:20 +0200

    Added strong parameters to admin articles.

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -3,16 +3,30 @@ class Admin::ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def edit
-    @article = Article.find params[:id]
-  end
-
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new article_params
+    if @article.save
+      redirect_to [:edit, :admin, @article]
+    else
+      render(action: :new) 
+    end
+  end
+
+  def edit
+    @article = Article.find params[:id]
   end
 
   def destroy
     @article = Article.find params[:id]
     @article.destroy
+  end
+private
+  def article_params
+    logger.debug '#'*100
+    params.require(:article).permit(:title, :content)
   end
 end
