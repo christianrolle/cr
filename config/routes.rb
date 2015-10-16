cf92841f94e20626bc676d549bddcc71f8e26c14
Christian Rolle <rolle.christian@gmail.com>
2015-10-16 16:38:46 +0200

    Changes root url from welcome controller to articles ressource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   match 'login', to: "user_sessions#new", via: :get
   match 'logout', to: "user_sessions#destroy", via: :get
-  root 'welcome#index'
+#  root 'welcome#index'
+  root 'articles#index'
 
   resources :user_sessions, only: :create
   resources :articles, path: 'blog', only: [:index, :show]
