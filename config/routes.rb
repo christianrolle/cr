cef1190f879756986548fa36a689dbfaa10f0265
Christian Rolle <rolle.christian@gmail.com>
2015-10-27 21:14:40 +0100

    Adds routes to Tag collection and creating/deleting Article Tags in the Admin scope.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   resources :user_sessions, only: :create
   resources :articles, path: 'blog', only: [:index, :show]
+  resources :tags, only: :index
   namespace :admin do
     resources :articles, except: :show
+    resources :article_tags, only: [:create, :destroy]
   end
 end
