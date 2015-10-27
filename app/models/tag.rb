45f45df0993184457791975f7925ba2cb4f8a05a
Christian Rolle <rolle.christian@gmail.com>
2015-10-27 21:16:38 +0100

    Adds named scope for ordering tags by their name ascending.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
+  scope :ordered, order('name ASC')
+
   def self.search(term=nil)
     return scoped if term.blank?
     where 'name LIKE :term', { term: "%#{term}%" }
