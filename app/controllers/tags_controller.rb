0b0c044d020628438412d225d1e699e1d2a2ab1c
Christian Rolle <rolle.christian@gmail.com>
2015-10-27 21:17:29 +0100

    Returns all searched Tags ordered by their name ascending.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.search(params[:search]).all
+    @tags = Tag.search(params[:search]).ordered.all
     render json: @tags
   end
 end
