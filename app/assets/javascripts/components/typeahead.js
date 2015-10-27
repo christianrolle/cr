4b24220ee55a8d3fcd67543221fecd6e6909cad3
Christian Rolle <rolle.christian@gmail.com>
2015-10-27 21:11:49 +0100

    Observes typeahead with search parameters to all input fields having class typeahead.

---
+++ app/assets/javascripts/components/typeahead.js
@@ -0,0 +1,22 @@
+/* Bootstrap autocompleter default configuration start */
+$.fn.typeahead.defaults['source'] = function(text, process){
+  var searchUrl = $(this.$element).attr("data-search");
+  $.get(searchUrl, { search: text })
+    .done( function(data){ return process(data) } );
+}
+/* Bootstrap autocompleter default configuration end */
+jQuery(document).ready(function(){
+  autocomplete('.typeahead');
+});
+
+/* Observe bootstrap autocompleter start */
+function autocomplete(selector){
+  $(selector).each(function(id, element){
+    $(element).one('focus', function(event){
+      $(this).typeahead({
+//        updater: autocomplete_submitter
+      });
+    });
+  });
+}
+/* Observe bootstrap autocompleter end */
