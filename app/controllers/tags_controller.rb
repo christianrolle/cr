5b39bd914a26a0dbd762681e7b68aee39f7b7825
Christian Rolle <rolle.christian@gmail.com>
2015-10-30 21:52:22 +0100

    Renders responding Tag JSON only with id and name. At the moment the search is requested by a typeahead field and submitting the selected Tag, only requires id. Though the Tag name is required for naming the resource.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
     @tags = Tag.search(params[:search]).ordered.all
-    render json: @tags
+    render json: @tags.as_json(only: [:id, :name])
   end
 end
