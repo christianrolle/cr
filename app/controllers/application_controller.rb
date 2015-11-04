8f53ebd25928c78c0af705d123ec763d3779167e
Christian Rolle <rolle.christian@gmail.com>
2015-11-04 22:50:01 +0100

    Only accepts de and en as localization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,13 @@ private
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 
+  def accepted_locale
+    return 'de' if extract_locale_from_accept_language_header.eql?('de')
+    I18n.default_locale
+  end
+
   def set_locale
-    I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
+    I18n.locale = accepted_locale
   end
 
   def current_user_session
