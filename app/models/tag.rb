613bfe305e8afeb0a278aaba48933f7c921b6c49
Christian Rolle <rolle.christian@gmail.com>
2015-11-06 10:03:19 +0100

    Validates uniqueness and presence of name.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+
   scope :ordered, -> { order('name ASC') }
   scope :excluding, -> (tag_ids=[]) { where.not id: tag_ids if tag_ids.any? }
   scope :search, -> (term=nil) { 
