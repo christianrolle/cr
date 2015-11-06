214324a6d04e767552723df7e3852c9599f86436
Christian Rolle <rolle.christian@gmail.com>
2015-11-06 09:35:31 +0100

    Renders modal dialog partial by using it as layout and injecting the content and title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def gravatar_url size=32
     "http://www.gravatar.com/avatar/c1873b0d2c86671bb1b3d44b53b48c09.png?s=#{size}"
   end
+  
+  def modal title, content
+    render layout: 'shared/modal', locals: { title: 'Tags' } do
+      content
+    end
+  end
 end
