07aa0ed2b272b994c5283b5b540f80d1925f6554
Christian Rolle <rolle.christian@gmail.com>
2015-11-06 09:55:23 +0100

    Adds REST API actions for editing, updating, creating and destroying Tags in Admin scope.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,7 +1,39 @@
 class Admin::TagsController < ApplicationController
   def index
-    article = Article.find params[:article_id]
-    @tags = Tag.search(params[:search]).excluding(article.tag_ids).ordered.all
-    render json: @tags.as_json(only: [:id, :name])
+    respond_to do |format|
+      format.json do 
+        article = Article.find params[:article_id]
+        @tags = Tag.search(params[:search])
+                    .excluding(article.tag_ids)
+                    .ordered.all
+        render json: @tags.as_json(only: [:id, :name])
+      end
+      format.js { @tags = Tag.ordered.all }
+    end
+  end
+
+  def edit
+    @tag = Tag.find params[:id]
+  end
+
+  def create
+    @tag = Tag.new tag_params
+    @tag.save!
+  end
+
+  def update
+    @tag = Tag.find params[:id]
+    @tag.attributes = tag_params
+    @tag.save!
+  end
+
+  def destroy
+    @tag = Tag.find params[:id]
+    @tag.destroy
+  end
+
+private
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
