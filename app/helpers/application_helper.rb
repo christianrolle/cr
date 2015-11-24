9c3a3b423684a737ab34cd290225428101510f30
Christian Rolle <rolle.christian@gmail.com>
2015-11-24 11:23:33 +0100

    Serves gravatar image over SSL as well.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def gravatar_url size=32
-    image_path "http://www.gravatar.com/avatar/c1873b0d2c86671bb1b3d44b53b48c09.png?s=#{size}"
+    image_path "https://www.gravatar.com/avatar/c1873b0d2c86671bb1b3d44b53b48c09.png?s=#{size}"
   end
   
   def modal title, content
