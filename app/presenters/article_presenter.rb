1311ad5023b32fca680428439a126f11d4c6c9b8
Christian Rolle <rolle.christian@gmail.com>
2015-11-27 16:42:57 +0100

    Localizes title, content and publishing date.

--- app/presenters/article_presenter.rb
+++ app/presenters/article_presenter.rb
@@ -1,11 +1,30 @@
 class ArticlePresenter < Presenter
   def tags_with_icon
+    tags = model.tags
+    return if tags.blank?
     css = 'list-unstyled tags glyphicon glyphicon-tag'
-    css << 's' if model.tags.size > 1
+    css << 's' if tags.size > 1
     h.content_tag(:ol, class: css) do
-      model.tags.inject(''.html_safe) { |html, tag|
+      tags.inject(''.html_safe) { |html, tag|
         html += h.content_tag(:li, tag.name)
       }
     end
+  end
+
+  def localized_or_default_title
+    title || model.title_en
+  end
+
+  def localized_publishing_date
+    return unless model.published?
+    I18n.l(model.published_at.to_date)
+  end
+
+  def content
+    model.read_attribute "content_#{I18n.locale}"
+  end
+
+  def title
+    model.read_attribute "title_#{I18n.locale}"
   end
 end
