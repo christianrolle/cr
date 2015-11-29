50a85ac1666ab1b96681d583cf259bfc1184c5bc
Christian Rolle <rolle.christian@gmail.com>
2015-11-29 22:52:17 +0100

    Validates titles and contents if published. The content of its localized title has to be present. The slug is localized but with primary language English and secondary language German.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,17 @@
 class Article < ActiveRecord::Base
-  before_validation :generate_slug
-
   has_many :article_tags, dependent: :delete_all
   has_many :tags, through: :article_tags
 
-  validates :title_en, uniqueness: true, length: { in: 3..200 }
-  validates :title_de, uniqueness: true, length: { in: 3..200 },
-    allow_blank: true
-  validates :slug, uniqueness: true
+  before_validation :set_slug, 
+    if: -> { published? && (title_en_changed? || title_de_changed?) }
+
+  validates :published_at, date: { allow_blank: true }
+  validates :title, presence: true, if: :published?
+  validates :title_en, :title_de, 
+    uniqueness: true, length: { in: 3..200 }, if: :published?, allow_nil: true
+  validates :slug, uniqueness: true, allow_nil: true
+  validates :content_en, presence: true, if: -> { published? && title_en.present? }
+  validates :content_de, presence: true, if: -> { published? && title_de.present? }
   validate :has_at_least_one_tag, if: :published?
 
   scope :published, -> { where.not(published_at: nil) }
@@ -15,6 +19,15 @@ class Article < ActiveRecord::Base
   scope :unpublished_first, -> { order('published_at IS NULL DESC') }
   scope :by_creation, -> { order('created_at ASC') }
   scope :by_publishing, -> { order('published_at DESC') }
+
+  def write_attribute name, value
+    return if value.blank?
+    super(name, value)
+  end 
+
+  def title
+    title_en || title_de
+  end
 
   def published_on
     published_at.to_date
@@ -24,8 +37,8 @@ class Article < ActiveRecord::Base
     published_at.present?
   end
   
-  def generate_slug
-    self.slug = title_en.parameterize
+  def set_slug
+    self.slug = title.parameterize
   end
 
   private
