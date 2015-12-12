a0459c9cf37c394eed1dbe2a4faa509d547a1e54
Christian Rolle <rolle.christian@gmail.com>
2015-12-12 02:29:28 +0100

    Refactored validating title, slug and published_at. Furthermore setting the slug was refactored. It can be reset to nil.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,13 +2,13 @@ class Article < ActiveRecord::Base
   has_many :article_tags, dependent: :delete_all
   has_many :tags, through: :article_tags
 
-  before_validation :set_slug 
+  before_validation :set_slug
 
-  validates :published_at, date: { allow_blank: true }
-  validates :title, presence: true, allow_blank: false
+  validates :published_at, date: { allow_nil: true }
+  validates :title_en_or_de, presence: true
   validates :title_en, :title_de, 
     uniqueness: true, length: { in: 3..200 }, allow_blank: true
-  validates :slug, uniqueness: true
+  validates :slug, uniqueness: true, allow_nil: true
   validates :content_en, presence: true, if: -> { published? && title_en.present? }
   validates :content_de, presence: true, if: -> { published? && title_de.present? }
   validate :has_at_least_one_tag, if: :published?
@@ -19,10 +19,6 @@ class Article < ActiveRecord::Base
   scope :by_creation, -> { order('created_at ASC') }
   scope :by_publishing, -> { order('published_at DESC') }
 
-  def title
-    title_en || title_de
-  end
-
   def published_on
     published_at.to_date
   end
@@ -30,17 +26,28 @@ class Article < ActiveRecord::Base
   def published?
     published_at.present?
   end
-  
-  def set_slug
-    self.slug = title.parameterize if title.present?
-  end
 
-  private
+private
+  def set_slug
+    title = title_en_or_de
+    slug = title.parameterize if title.present?
+    self.slug = slug
+  end
+=begin
   def has_at_least_one_tag
-    return if tags.any?
+    return true if tags.any?
+    errors.add(:tags, :at_least_one)
+  end
+=end
+  def has_at_least_one_tag
+    return true if tags.any?
     message = I18n.t('errors.format', 
         attribute: self.class.human_attribute_name(:tags), 
         message: I18n.t('errors.messages.at_least_one'))
     errors.add(:base, message)
+  end
+
+  def title_en_or_de
+    title_en || title_de
   end
 end
