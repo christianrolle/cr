17769e69bd9f3e2dcb82de90612566ca470eb308
Christian Rolle <rolle.christian@gmail.com>
2015-12-22 09:09:41 +0100

    Removes legacy comments and adds syntax highlighter theme 'github'.

--- app/models/concerns/markdown_service.rb
+++ app/models/concerns/markdown_service.rb
@@ -1,17 +1,4 @@
 class MarkdownService
-=begin
-  class Renderer < Redcarpet::Render::HTML
-    include Rouge::Plugins::Redcarpet
-    protected
-    def rouge_formatter(opts = {})
-      options = { 
-        css_class:    'highlight', 
-        line_numbers: true
-      }
-      Rouge::Formatters::HTML.new options
-    end
-  end
-=end
   attr_reader :markdown
 
   def self.call(markdown)
@@ -29,20 +16,15 @@ class MarkdownService
   private
   def markdown_renderer
     options = {
-      autolink:           true, 
-      tables:             true,
-      smartypants:        true,
-      fenced_code_blocks: true }
-    #Redcarpet::Markdown.new(Renderer, options)
-    options = {
       syntax_highlighter: 'rouge',
+      syntax_highlighter_opts: { inline_theme: 'github' },
       coderay_line_numbers: :table
     }
     Kramdown::Document.new(markdown, options)
   end
 
   def render
-    #markdown_renderer.render(markdown).html_safe
     markdown_renderer.to_html
   end
 end
+
