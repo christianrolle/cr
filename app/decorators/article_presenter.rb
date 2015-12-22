c371cbcf7c8253bfc555ea5aef9b5d29689c0302
Christian Rolle <rolle.christian@gmail.com>
2015-12-22 10:04:27 +0100

    Adds social share urls for Twitter and Google+.

--- app/decorators/article_presenter.rb
+++ app/decorators/article_presenter.rb
@@ -1,4 +1,9 @@
 class ArticlePresenter < Presenter
+  SOCIAL_SHARE_URLS = { 
+    twitter: 'http://twitter.com/home?status',
+    google_plus: 'https://plus.google.com/share?url'
+  }
+
   def tags_with_icon
     tags = model.tags
     return if tags.blank?
@@ -10,15 +15,12 @@ class ArticlePresenter < Presenter
       }
     end
   end
-=begin
-  def tags_with_icon
-    tags = model.tags
-    return if tags.blank?
-    css = 'list-unstyled tags glyphicon glyphicon-tag'
-    css << 's' if tags.size > 1
-    h.content_tag(:ol, class: css) do
-      tags.map { |tag| h.content_tag(:li, tag.name) }
-    end
+
+  def social_share_url social_service
+    "#{SOCIAL_SHARE_URLS[social_service]}=#{article_url}"
   end
-=end
+private
+  def article_url
+    @article_url ||= h.article_url(model)
+  end
 end
