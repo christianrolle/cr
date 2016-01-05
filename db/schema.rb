be748b4abea322ae515179a2e8261fd274492f3e
Christian Rolle <rolle.christian@gmail.com>
2016-01-05 07:59:51 +0100

    Adds localized summary_de and summary_en to articles.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127133536) do
+ActiveRecord::Schema.define(version: 20160105065523) do
 
   create_table "article_tags", force: :cascade do |t|
     t.integer "article_id"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20151127133536) do
     t.string   "issue"
     t.string   "title_de"
     t.text     "content_de"
+    t.text     "summary_de"
+    t.text     "summary_en"
   end
 
   create_table "blog_posts", force: :cascade do |t|
