b7c25322c025bc81fd8e5b55f08c32958b511c32
Christian Rolle <rolle.christian@gmail.com>
2016-01-05 08:58:19 +0100

    Fixes missing Turbolinks workaround for initialising typeahead field for tag search after focusing it.

--- app/assets/javascripts/admin/articles.js
+++ app/assets/javascripts/admin/articles.js
@@ -1,12 +1,15 @@
-jQuery(document).ready(function(){
+$(document).on('page:change', function(){
   $('#articles .tag.search').one('focus', function(evt){
+    console.log('Focused')
     $(this).typeahead({
       source: function(text, process){
+    console.log('typeahead')
         var tag_ids = []
         $('.tags input').each(function(){
           tag_ids.push($(this).val())
         })
         var searchUrl = $(this.$element).attr("data-search");
+    console.log(searchUrl)
         $.get(searchUrl, { search: text, "tag_ids[]": tag_ids })
             .done( function(data){ return process(data) } );
       },
