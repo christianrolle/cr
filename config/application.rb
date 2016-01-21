4d789100a40137cf4ecb4743d6016dbd1d518b1c
Christian Rolle <rolle.christian@gmail.com>
2016-01-21 09:55:05 +0100

    Removes SSL security, since Heroku can't deal with ARecords. :(

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Cr
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    config.force_ssl = true
+    #config.force_ssl = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/decorators) 
