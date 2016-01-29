ea0b805a77fd9afe5512cb2d5926d43f996c72b2
Christian Rolle <rolle.christian@gmail.com>
2016-01-29 15:35:37 +0100

    Adds link to avatar image, since some logic has to removed out of the view.

--- app/decorators/article_presenter.rb
+++ app/decorators/article_presenter.rb
@@ -11,6 +11,14 @@ class ArticlePresenter < Presenter
     end
   end
 
+  def link_to_avatar
+    return h.content_tag(:div, '', class: :avatar) if model.new_record?
+    avatar = model.avatar
+    image = h.image_tag(avatar.url(:medium)) if avatar.present?
+    h.link_to image, h.edit_admin_article_avatar_path(model), remote: true, 
+      class: :avatar
+  end
+  
   def url
     @url ||= h.article_url(model.slug)
   end
