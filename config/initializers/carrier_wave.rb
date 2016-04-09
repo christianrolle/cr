ed2a7d26fe992f76c2de52b65bb247dbd42fff58
Christian Rolle <rolle.christian@gmail.com>
2016-04-09 13:41:08 +0200

    Removed outdated fog configuration of AWS S3 access policy.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -18,7 +18,7 @@ CarrierWave.configure do |config|
     config.storage :file
   end
   config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.s3_access_policy = :public_read
+#  config.s3_access_policy = :public_read
 #  config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 #  config.fog_attributes = { 'Cache-Control' => 'max-age=#{365.say.to_i}' }
 end
