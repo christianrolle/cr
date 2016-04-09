19628c3b3054330c737b9c267a88fa46e6bc037e
Christian Rolle <rolle.christian@gmail.com>
2016-04-09 13:08:28 +0200

    Configures carrierwave for usage in Heroku with AWS S3.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,1 +1,25 @@
 require 'carrierwave/orm/activerecord'
+CarrierWave.configure do |config|
+
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    :provider               => 'AWS', # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], # required
+#    :host                   => 's3.example.com', # optional, defaults to nil
+#    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = ENV['S3_BUCKET_NAME'] # required
+#  config.fog_public     = false # optional, defaults to true
+  if Rails.env.production?
+    config.storage :fog
+    #config.root = Rails.root.join('tmp')
+    #config.cache_dir = 'uploads'
+  else
+    config.storage :file
+  end
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.s3_access_policy = :public_read
+#  config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+#  config.fog_attributes = { 'Cache-Control' => 'max-age=#{365.say.to_i}' }
+end
