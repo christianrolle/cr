5a8c33483b2e1dbfcdae6a1f1ab63c9dc4137a59
Christian Rolle <rolle.christian@gmail.com>
2016-05-09 15:56:32 +0200

    Layout Helper with tag generating helper methods for inline CSS elements (used in the mail signature).

---
+++ lib/layout/inline_helper.rb
@@ -0,0 +1,65 @@
+module Layout
+  module InlineHelper
+  
+    def b value, styles={}
+      css = serialize(text styles)
+      content_tag(:b, value, style: css)
+    end
+
+    def span value, styles={}
+      css = serialize(text styles)
+      content_tag(:span, value, style: css)
+    end
+
+    def div value, styles={}
+      css = serialize(text styles)
+      content_tag(:div, value, style: css)
+    end
+
+    def td styles={}
+      css = serialize(table_cell styles)
+      content_tag(:td, valign: 'top', style: css) { yield }
+    end
+
+    def icon label
+      image_tag "#{label}.png", alt: label, nosend: 1, border: 0, 
+        style: 'opacity: 0.7;'
+    end
+
+    def link_to_icon label, url
+      link_to icon(label), url, style: text_styles('color' => '#333333')
+    end
+
+    private
+
+    def serialize styles={}
+      styles.map { |style, value| "#{style}: #{value};" }
+    end
+
+    def text_styles styles={}
+      styles['color'] ||= '#000000'
+      styles['font-family'] ||= "'Arial', sans-serif"
+      styles['font-size'] ||= '10pt'
+      styles['text-align'] ||= 'left'
+      serialize styles
+    end
+
+    def text styles={}
+      styles['color'] ||= '#000000'
+      styles['font-family'] ||= "'Arial', sans-serif"
+      styles['font-size'] ||= '10pt'
+      styles['text-align'] ||= 'left'
+      styles
+    end
+
+    def table_cell styles={}
+      styles['padding-top'] ||= '0px'
+      styles['padding-right'] ||= '0px'
+      styles['padding-bottom'] ||= '0px'
+      styles['padding-left'] ||= '0px'
+      styles['text-align'] ||= 'left'
+      styles
+    end
+
+  end
+end
