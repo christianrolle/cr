b0759463be09bf1ae0d679e739b74deb30a47560
Christian Rolle <rolle.christian@gmail.com>
2016-05-09 12:06:14 +0200

    Renders documents depending on the layout parameter, with the requested layout. There is no layout, if the parameter contains 'plain'.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,7 +1,20 @@
 class DocumentsController < ApplicationController
 
   def show
-    render action: params[:document]
+#    render action: , layout: layout
+    render_document layout
+  end
+
+  private
+
+  def render_document layout=nil
+    render action: params[:document].underscore, layout: layout
+  end
+
+  def layout
+    return true if params[:layout].nil?
+    return false if params[:layout] == 'plain'
+    params[:layout]
   end
 
 end
