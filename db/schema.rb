ce5dacb760b286e2a9a58617501958a1e66abd5b
Christian Rolle <rolle.christian@gmail.com>
2016-05-11 14:56:59 +0200

    Current schema.

--- db/schema.rb
+++ db/schema.rb
@@ -22,39 +22,15 @@ ActiveRecord::Schema.define(version: 20160409093522) do
     t.string   "title_en"
     t.text     "content_en"
     t.datetime "published_at"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "slug"
-    t.string   "issue"
     t.string   "title_de"
     t.text     "content_de"
     t.text     "summary_de"
     t.text     "summary_en"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.string   "image"
   end
-
-  create_table "blog_posts", force: :cascade do |t|
-    t.datetime "published_at", null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
-    t.datetime "created_at"
-  end
-
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
