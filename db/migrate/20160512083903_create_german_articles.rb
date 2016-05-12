f368819966009b476d07ccb210d8e2699653e1c6
Christian Rolle <rolle.christian@gmail.com>
2016-05-12 15:01:12 +0200

    Wraps a trasnaction around the data migration.

--- db/migrate/20160512083903_create_german_articles.rb
+++ db/migrate/20160512083903_create_german_articles.rb
@@ -1,28 +1,32 @@
 class CreateGermanArticles < ActiveRecord::Migration
   def change
-    Article.all.each do |article|
-      german_article = article.dup
-      german_article.locale = :de
-      article.locale = :en
+    Article.transaction do
+      Article.all.each do |article|
+        german_article = article.dup
+        german_article.locale = :de
+        article.locale = :en
 
-      german_article.slug = nil
+        german_article.slug = nil
 
-      article.title_de = nil
-      article.content_de = nil
-      article.summary_de = nil
-      german_article.title_en = german_article.title_de
-      german_article.content_en = german_article.content_de
-      german_article.summary_en = german_article.summary_de
-      german_article.title_de = nil
-      german_article.content_de = nil
-      german_article.summary_de = nil
+        article.title_de = nil
+        article.content_de = nil
+        article.summary_de = nil
+        german_article.title_en = german_article.title_de
+        german_article.content_en = german_article.content_de
+        german_article.summary_en = german_article.summary_de
+        german_article.title_de = nil
+        german_article.content_de = nil
+        german_article.summary_de = nil
 
-      attributes = { published_at: article.published_at }
-      article_supplement = article.create_article_supplement attributes
-      german_article.article_supplement = article_supplement
+        attribtues = { published_at: article.published_at }
+        article_supplement = ArticleSupplement.create! attributes
 
-      article.save!
-      german_article.save!
+        article.article_supplement = article_supplement
+        german_article.article_supplement = article_supplement
+
+        article.save!
+        german_article.save!
+      end
     end
   end
 end
