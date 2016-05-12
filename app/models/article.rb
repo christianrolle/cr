30aade07f6b40f7889f1d14f8b3e9d1b13033ba0
Christian Rolle <rolle.christian@gmail.com>
2016-05-12 12:18:29 +0200

    Renames all locale specific references to the more general alternatives (title_en to title, content_en to text).

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,30 +8,28 @@ class Article < ActiveRecord::Base
 
   before_validation :set_slug
 
-  validates :title_en, presence: true
-  validates :title_en, 
+  validates :title, presence: true
+  validates :title, 
     uniqueness: { scope: :locale }, 
     length: { in: 3..200 }, 
     allow_blank: true
   validates :slug, uniqueness: true, allow_nil: true
-  validates :content_en, presence: true, if: -> { published? && title_en.present? }
+  validates :text, presence: true, if: -> { published? && title.present? }
 
-  scope :localized, ->(locale) { where.not("title_#{locale}" => nil) }
+  scope :localized, -> { where.not("title" => nil) }
   scope :by_creation, -> { order('created_at ASC') }
   scope :search, ->(term) {
     term = term.to_s.strip
     return if term.empty?
-    where("title_en LIKE :term OR title_de LIKE :term", { term: "%#{term}%" })
+    where("title LIKE ?", "%#{term}%")
   }
-  scope :locale, ->(locale) { select("title_#{locale}, content_#{}") }
 
   delegate :published?, to: :article_supplement
+
   private
 
   def set_slug
-    title = title_en
-    slug = title.parameterize if title.present?
-    self.slug = slug
+    self.slug = title.parameterize if title.present?
   end
 
 end
