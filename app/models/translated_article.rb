d872ecb7d47ec669903251ccc1ce8c074e94d504
Christian Rolle <rolle.christian@gmail.com>
2016-05-13 15:13:11 +0200

    Wires up translated article with its associations to tags. Defines locale enums explicitely, for leaving undefined articles with zero. Adds scopes for searching, localizing and ordering translated articles.

--- app/models/translated_article.rb
+++ app/models/translated_article.rb
@@ -2,11 +2,13 @@ class TranslatedArticle < ActiveRecord::Base
 
   mount_uploader :image, ArticleImageUploader
 
-  enum locale: [:de, :en]
+  enum locale: { en: 1, de: 2 }
 
   belongs_to :article
+  has_many :article_tag_positions, through: :article
+  has_many :tags, through: :article_tag_positions
 
-  before_validation :set_slug, unless: :published?
+  before_validation :set_slug
 
   validates :title, 
     uniqueness: { scope: :locale }, 
@@ -17,7 +19,15 @@ class TranslatedArticle < ActiveRecord::Base
     allow_nil: true
   validates :text, presence: true, if: -> { published? && title.present? }
 
-  delegate :published?, to: :article
+  scope :localized, ->(locale) { where(locale: locales[locale.to_s]) }
+  scope :search, ->(term) {
+    term = term.to_s.strip
+    return if term.empty?
+    where("title LIKE ?", "%#{term}%")
+  }
+  scope :by_publishing, -> { eager_load(:article).merge(Article.by_publishing) }
+
+  delegate :published?, :published_at, to: :article
 
   private
 
