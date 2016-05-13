e0db4abb54a1707b6ca1aa167620801a8a01206a
Christian Rolle <rolle.christian@gmail.com>
2016-05-13 15:15:23 +0200

    Moved scopes into translated article. Distingueshes between publishing and releasing. A translated article can be released, but not been published, if the publishing date is before 'today'.

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,14 +4,13 @@ class Article < ActiveRecord::Base
 
   enum locale: [:de, :en]
 
-  belongs_to :article_supplement
   # TODO: legacy association start - best before: 15.05.2016
   has_many :article_tags, dependent: :delete_all
   has_many :legacy_tags, through: :article_tags, class_name: Tag
   # legacy association stop
-  has_many :article_tag_positions, dependent: :delete_all
+  has_many :article_tag_positions, -> { order('article_tag_positions.position ASC') },
+    dependent: :delete_all
   has_many :tags, through: :article_tag_positions
-
 
   before_validation :set_slug
 
@@ -22,27 +21,16 @@ class Article < ActiveRecord::Base
   validates :slug, 
     uniqueness: { scope: :locale }, 
     allow_nil: true
-  #validates :text, presence: true, if: -> { published? && title.present? }
-  validates :content_en, presence: true, if: -> { published? && title.present? }
+  validates :content_en, presence: true, if: -> { released? && title.present? }
 
-  scope :localized, ->(locale) { where(locale: locales[locale.to_sym]) }
-  scope :with_tags, -> { includes(article_supplement: :tags) }
   scope :by_creation, -> { order('created_at ASC') }
-  scope :by_publishing, -> { 
-    includes(:article_supplement)
-      .merge(ArticleSupplement.published)
-      .merge(ArticleSupplement.by_publishing) 
-  }
-  scope :search, ->(term) {
-    term = term.to_s.strip
-    return if term.empty?
-    where("title LIKE ?", "%#{term}%")
-  }
+  scope :published, -> { where("published_at < ?", Time.current) }
+  scope :by_publishing, -> { published.order('published_at DESC') }
 
-#  delegate :published?, to: :article_supplement
-  def published?
-    title.present? && content_en.present?
+  def released?
+    published_at.present?
   end
+
   private
 
   def set_slug
