ecb3031e535190d907a81984d516cc7a56b30d58
Christian Rolle <rolle.christian@gmail.com>
2016-05-13 15:01:21 +0200

    Enforces setting german slug.

--- db/migrate/20160512083903_create_german_articles.rb
+++ db/migrate/20160512083903_create_german_articles.rb
@@ -13,22 +13,22 @@ class CreateGermanArticles < ActiveRecord::Migration
 
         article_attributes = {
           article: legacy_article,
-          image: legacy_article.image,
           created_at: legacy_article.created_at,
           updated_at: legacy_article.updated_at
         }
         english_article = TranslatedArticle.new article_attributes
-        english_article.locale = :en
-        english_article.title = legacy_article.title_en
-        english_article.text = legacy_article.content_en
+        english_article.locale  = :en
+        english_article.title   = legacy_article.title_en
+        english_article.text    = legacy_article.content_en
         english_article.summary = legacy_article.summary_en
-        english_article.slug = legacy_article.slug
+        english_article.slug    = legacy_article.slug
 
         german_article = TranslatedArticle.new article_attributes
-        german_article.locale = :de
-        german_article.title = legacy_article.title_de
-        german_article.text = legacy_article.content_de
-        german_article.summary = legacy_article.summary_de
+        german_article.locale   = :de
+        german_article.title    = legacy_article.title_de
+        german_article.text     = legacy_article.content_de
+        german_article.summary  = legacy_article.summary_de
+        german_article.slug     = nil
         
         english_article.save!
         german_article.save!
