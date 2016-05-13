7b5abd455180be1c3a44bb85bba6e36ca1f9dc72
Christian Rolle <rolle.christian@gmail.com>
2016-05-13 12:56:03 +0200

    Legacy model for ArticleTag. Has to removed in next deployment step.

--- app/models/article_tag.rb
+++ app/models/article_tag.rb
@@ -2,7 +2,7 @@ class ArticleTag < ActiveRecord::Base
   belongs_to :tag, counter_cache: true
   belongs_to :article
 
-  validates :article, :tag, presence: true
+#  validates :article, :tag, presence: true
 
   delegate :name, to: :tag
 end
