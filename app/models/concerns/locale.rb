3879add768b0def9348dd151956a602d1f7e7814
Christian Rolle <rolle.christian@gmail.com>
2016-05-15 21:45:46 +0200

    Adds accessor for alternate url. The alternate url is the other of de or en localized url (but the same resource).

--- app/models/concerns/locale.rb
+++ app/models/concerns/locale.rb
@@ -1,4 +1,7 @@
 class Locale
+
+  attr_accessor :alternate_url
+
   def initialize locale
     @locale = self.class.accept_or_default locale.to_sym
   end
@@ -14,9 +17,12 @@ class Locale
   def self.available
     I18n.available_locales
   end
-private
+
+  private
+
   def self.accept_or_default locale
     return locale if available.include?(locale)
     I18n.default_locale
   end
+
 end
