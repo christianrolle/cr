d30d969c28b8bdaa311afe3d4397a118470678bf
Christian Rolle <rolle.christian@gmail.com>
2016-05-15 21:53:07 +0200

    Sets the translated url according to the translated article slug.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = TranslatedArticle.localized(locale)
                                   .search(params[:search])
@@ -8,5 +9,18 @@ class ArticlesController < ApplicationController
 
   def show
     @article = TranslatedArticle.find_by_slug params[:id]
+    alternate_to alternate_url(@article)
+  end
+
+  private
+
+  def alternate_url article
+    alternate_slug = alternate_slug(article)
+    return if alternate_slug.nil?
+    url_for(params.merge({ locale: @locale.secondary, id: alternate_slug }))
+  end
+
+  def alternate_slug article
+    article.translated_articles.localized(@locale.secondary).pluck(:slug).first
   end
 end
