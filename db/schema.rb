02f7d3ff100ba17b5edebda7ffc3608116eeba86
Christian Rolle <rolle.christian@gmail.com>
2016-05-17 08:49:48 +0200

    Contains the moved image url from the article translation to the article itself.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512214740) do
+ActiveRecord::Schema.define(version: 20160517064202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160512214740) do
     t.datetime "published_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "image"
   end
 
   create_table "tags", force: :cascade do |t|
