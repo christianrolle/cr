eea7306422ae590937c9b6353aee25aec4518ebd
Christian Rolle <rolle.christian@gmail.com>
2016-05-25 14:39:36 +0200

    Delegates title and slug to the related article.

--- app/models/article_relation.rb
+++ app/models/article_relation.rb
@@ -3,6 +3,7 @@ class ArticleRelation < ActiveRecord::Base
   enum kind: { next: 1, previous: 2 }
 
   belongs_to :article
-  belongs_to :related_article, class_name: Article
+  belongs_to :related_article, class_name: Article 
 
+  delegate :title, :slug, to: :related_article
 end
