85d525436b06f533b96f72fdd609ee73acb4106d
Christian Rolle <rolle.christian@gmail.com>
2016-05-25 14:35:59 +0200

    Removes slug related functionality, since it was moved to the article iteself.

--- app/models/translated_article.rb
+++ app/models/translated_article.rb
@@ -2,25 +2,14 @@ class TranslatedArticle < ActiveRecord::Base
 
   enum locale: { en: 1, de: 2 }
   
-  LOCALIZABLE_ATTRIBUTES = %w(title text summary slug)
-
   belongs_to :article
   has_many :article_tag_positions, through: :article
   has_many :tags, through: :article_tag_positions
-#  has_many :translated_articles, ->(translated_article) { 
-#      where("#{table_name}.id != ?", translated_article.id) 
-#    },
-#    through: :article 
-
-  before_validation :set_slug
 
   validates :title, 
     uniqueness: { scope: :locale }, 
     length: { in: 3..200 }, 
     allow_blank: true
-  validates :slug, 
-    uniqueness: { scope: :locale }, 
-    allow_nil: true
   validates :text, presence: true, if: -> { released? && title.present? }
 
   scope :localized, ->(locale) { 
@@ -34,17 +23,7 @@ class TranslatedArticle < ActiveRecord::Base
     return if term.empty?
     where("title LIKE ?", "%#{term}%")
   }
-  scope :select_localizeables, -> { 
-    attributes = LOCALIZABLE_ATTRIBUTES.map { |name| "#{table_name}.#{name}" }
-    select(attributes.join(', '))
-  }
 
   delegate :released?, :published_at, :image, to: :article
-
-  private
-
-  def set_slug
-    self.slug ||= title.parameterize if title.present?
-  end
 
 end
