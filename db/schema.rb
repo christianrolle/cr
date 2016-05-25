891a4e98175860ed3d172bc5c367be1a6bc10c02
Christian Rolle <rolle.christian@gmail.com>
2016-05-25 14:34:55 +0200

    Moves slugs from article translation to article. Article slugs are not translated anymore.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519134338) do
+ActiveRecord::Schema.define(version: 20160520120441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
