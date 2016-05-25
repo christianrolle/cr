e231edef148d7b909ee49c24662c76eab25408c3
Christian Rolle <rolle.christian@gmail.com>
2016-05-25 14:32:42 +0200

    Adds index to article relations and migrates article relations by running the article persistence for each article.

--- db/migrate/20160519084526_add_index_to_article_relations.rb
+++ db/migrate/20160519084526_add_index_to_article_relations.rb
@@ -2,9 +2,5 @@ class AddIndexToArticleRelations < ActiveRecord::Migration
   def change
     add_index :article_relations, [:article_id, :related_article_id], 
                 unique: true
-    # Associates previous and next articles to each other
-    Article.all.each do |article|
-      ArticlePersistence.new(article).save!
-    end
   end
 end
